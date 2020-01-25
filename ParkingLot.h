6cf4eee9e2c7d76f50d4686ad0b46de8b8bf0ba8
Gelasnik <58441581+Gelasnik@users.noreply.github.com>
2020-01-25 14:45:50 +0200

    Update ParkingLot.h

--- ParkingLot.h
+++ ParkingLot.h
@@ -7,6 +7,17 @@
 #include "UniqueArray.h"
 namespace MtmParkingLot {
 
+    typedef unsigned int cash;
+
+    const cash FINE_PRICE = 250;
+    const unsigned int MOTORBIKE_MAX_HOURS_TO_PAY = 6;
+    const cash MOTORBIKE_FIRST_HOUR_PRICE = 10;
+    const cash MOTORBIKE_EXTRA_HOUR_PRICE = 5;
+    const unsigned int CAR_MAX_HOURS_TO_PAY = 6;
+    const cash CAR_FIRST_HOUR_PRICE = 20;
+    const cash CAR_EXTRA_HOUR_PRICE = 10;
+    const cash HANDICAPPED_CONSTANT_PRICE = 15;
+
     using namespace ParkingLotUtils;
     using std::ostream;
 
@@ -23,6 +34,8 @@ namespace MtmParkingLot {
         UniqueArray<Car, Compare> carParking;
         UniqueArray<Handicapped, Compare> handicappedParking;
         bool vehicleIsInParking(const ParkingLot::Vehicle& vehicle) const ;
+        const Vehicle *findVehicle(LicensePlate& basicString);
+        friend ostream& operator<<(ostream& os, const ParkingLot::Vehicle& vehicle);
     public:
 
         ParkingLot(unsigned int parkingBlockSizes[]);
@@ -32,6 +45,8 @@ namespace MtmParkingLot {
         ParkingResult getParkingSpot(LicensePlate licensePlate, ParkingSpot& parkingSpot) const;
         void inspectParkingLot(Time1 inspectionTime);
         friend ostream& operator<<(ostream& os, const ParkingLot& parkingLot);
+
+
 
     };
 
